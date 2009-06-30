b825c28e20039f281e69a1cecd01334952398f05
shock <billdoughty@capitalthought.com>
2009-06-30 12:04:10 -0500

    Check point commit.

--- sinatra/server.rb
+++ sinatra/server.rb
@@ -1,10 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
+i=0
+
 get '/hi' do
-  "Hello World!"
+  
+  output = "Hello World ##{i}!\n#{params.to_json}"
+  i = 0
+  output
 end
 
 
 get '/lo' do
   "HelLOW World!"
+end
+
+t = Thread.new do
+  loop do
+    puts "#{i}"
+    sleep 1
+    i += 1
+  end
 end
